48f14456c3f4dd217eee1006bb77f85ddfeea224
KevinGuillaume <keving3492@gmail.com>
2025-11-27 22:51:59 -0600

    steps for getting polymarket in place

--- server/main.py
+++ server/main.py
@@ -18,4 +18,19 @@ def get_leage_info(league: str):
 
     response = requests.get(url, params=querystring)
 
-    return response.json()
+    pre_parsed_data = response.json()
+
+    # For each team get meta data
+    for d in pre_parsed_data:
+        #Then going to have to make a call to get meta deta
+
+
+        #After getting meta data gonna want to grab tags
+
+
+        #After getting tags then will want to use that to query markets 
+
+
+
+
+    return 
