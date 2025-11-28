07b8810fd8c12ca5c399edcb24ace8836d1d0f7a
KevinGuillaume <keving3492@gmail.com>
2025-11-28 17:19:10 -0600

    Add events related to the league

--- server/main.py
+++ server/main.py
@@ -9,26 +9,35 @@ app = FastAPI()
 def health():
     return {"Hello": "World"}
 
+# Gets all events related to a league
+def get_events_by_league(league: str):
+    """Returns all active polymarket events"""
+    url = "/events?closed=false&limit=500"
+    response = requests.get(url)
+
+    data = response.json()
+    print("length: ",len(data))
+    events = []
+    for event in data:
+        # Filter for NFL related events... NFL is hardcoded for now i'll add a map for different ones like nba soccer etc
+        if league in event["title"]:
+            events.append(event)
+
+    return events
+
 @app.get("/league/{league}")
 def get_leage_info(league: str):
     """Returns all teams associated with a given league"""
     url = "/teams"
 
-    querystring = {"league":"nfl"}
-
+    querystring = {
+        "league":league
+    }
+    all_event = get_events_by_league(league)
     response = requests.get(url, params=querystring)
 
     pre_parsed_data = response.json()
 
-    # For each team get meta data
-    for d in pre_parsed_data:
-        #Then going to have to make a call to get meta deta
-
-
-        #After getting meta data gonna want to grab tags
-
-
-        #After getting tags then will want to use that to query markets 
 
 
 
