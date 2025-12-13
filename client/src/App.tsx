0e1bcb5d430be75260923bb1b5cff8f437e56940
KevinGuillaume <keving3492@gmail.com>
2025-12-12 18:30:13 -0600

    Adding react -router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import SidelineApp from "./SidelineApp";
 
 function App() {
-
   return (
-    <div>
-      Shadow Trader
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/app" element={<SidelineApp />} />
+        {/* Optional: redirect or 404 */}
+        {/* <Route path="*" element={<Navigate to="/" />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
