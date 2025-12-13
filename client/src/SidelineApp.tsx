ae8ac3e8c9f44c417dd6762d4bed6c3bfa7fe484
KevinGuillaume <keving3492@gmail.com>
2025-12-12 18:31:06 -0600

    Content page

---
+++ client/src/SidelineApp.tsx
@@ -0,0 +1,10 @@
+export default function SidelineApp() {
+    return (
+      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-6xl font-bold mb-8">Welcome to Sideline Trader</h1>
+          <p className="text-2xl text-gray-300">Here I will show you some cool stuff</p>
+        </div>
+      </div>
+    );
+  }
