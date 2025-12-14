5641de7ea3c9c5bdbdb9d195e7861ebeaa77755e
KevinGuillaume <keving3492@gmail.com>
2025-12-14 12:34:12 -0600

    adding api call to component

--- client/src/SidelineApp.tsx
+++ client/src/SidelineApp.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import { backendAPI } from "./api";
+
 export default function SidelineApp() {
+    const [data,setData] = useState(null)  
+
+    async function init(){
+        const values = await backendAPI.getEventsData("nfl")
+        setData(values)
+    }
+
+    useEffect(() => {
+        init()
+    },[])
+
+
     return (
       <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-6xl font-bold mb-8">Welcome to Sideline Trader</h1>
-          <p className="text-2xl text-gray-300">Here I will show you some cool stuff</p>
+          <p className="text-2xl text-gray-300">{data}</p>
         </div>
       </div>
     );
