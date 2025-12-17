597ab8e030f004c25d9301cf93bfee8aafcf65b3
KevinGuillaume <keving3492@gmail.com>
2025-12-17 12:12:54 -0600

    frontend to grab backend data correctly now

--- client/src/SidelineApp.tsx
+++ client/src/SidelineApp.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import { backendAPI } from "./api";
 
 export default function SidelineApp() {
-    const [data,setData] = useState(null)  
+    const [data,setData] = useState<any>([])  
 
     async function init(){
-        const values = await backendAPI.getEventsData("nfl")
-        setData(values)
+        const values = await backendAPI.getEventsData("NFL")
+        setData(values["events"])
     }
 
     useEffect(() => {
@@ -18,7 +18,10 @@ export default function SidelineApp() {
       <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-6xl font-bold mb-8">Welcome to Sideline Trader</h1>
-          <p className="text-2xl text-gray-300">{data}</p>
+          <div>
+            <p>All Questions:</p>
+            {data.map((e: any) => <p>{e.title}</p>)}
+          </div>
         </div>
       </div>
     );
