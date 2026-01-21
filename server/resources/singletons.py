24a28aa7a4b0ed32a723dae4e306325d2763e88d
KevinGuillaume <keving3492@gmail.com>
2026-01-20 21:01:17 -0600

    Supabase setup

--- server/resources/singletons.py
+++ server/resources/singletons.py
@@ -1,6 +1,12 @@
 from controllers.league_controller import LeagueController
 from controllers.player_controller import PlayerController
+from supabase import create_client, Client
+from configparser import ConfigParser
 
 
+_config = ConfigParser()
+_config.read("config.ini")
+
+supabase: Client = create_client(_config.get("SERVER","supabase_url"),_config.get("SERVER","supabase_key"))
 league_controller = LeagueController()
 player_controller = PlayerController()
--- server/routes/league_router.py
+++ server/routes/league_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from resources.singletons import league_controller
+from resources.singletons import supabase
 
 league_router = APIRouter(
     prefix="/api/v1/league",
@@ -14,6 +15,15 @@ async def get_league_markets(
     league: str
 ):
     """Gets all markets associated with a league"""
+    print("Supabase connection: ", supabase)
+    response = supabase.table('players').select("*").execute()
+    todos = response.data
+    print("Raw response:", response)
+    print("Data:", response.data)
+    print("Data length:", len(response.data))
+
+    for todo in todos:
+        print(todo)
     return await league_controller.get_markets_by_league(league)
 
 
