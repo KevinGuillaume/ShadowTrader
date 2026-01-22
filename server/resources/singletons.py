654c2ee4ec88230d09949f0d16b95fef000c6131
KevinGuillaume <keving3492@gmail.com>
2026-01-22 17:52:26 -0600

    db lookup updates

--- server/resources/singletons.py
+++ server/resources/singletons.py
@@ -8,5 +8,5 @@ _config = ConfigParser()
 _config.read("config.ini")
 
 supabase: Client = create_client(_config.get("SERVER","supabase_url"),_config.get("SERVER","supabase_key"))
-league_controller = LeagueController()
-player_controller = PlayerController()
+league_controller = LeagueController(supabase)
+player_controller = PlayerController(supabase)
